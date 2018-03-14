2ac4dd3cb17b778c8d90dd495baa9d517c89d20e
ruben bernardes <rubenmbernardes@gmail.com>
2018-03-14 12:36:06 +0100

    seeded

--- db/migrate/20180314110653_change_phone_number_data_type_to_restaurants.rb
+++ db/migrate/20180314110653_change_phone_number_data_type_to_restaurants.rb
@@ -2,6 +2,5 @@ class ChangePhoneNumberDataTypeToRestaurants < ActiveRecord::Migration[5.1]
   def change
     remove_column :restaurants, :phone_number
     add_column :restaurants, :phone_number, :string
-
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Super Mario',
+    address:      'Rua do Carmo',
+    phone_number:  '21 932 2334',
+    category:      'french'
+  },
+
+  {
+    name:         'Norte da China',
+    address:      'Cp. Ourique',
+    phone_number:  '21 932 2394',
+    category:      'chinese'
+  },
+
+  {
+    name:         'CasaNova',
+    address:      'Baixa',
+    phone_number:  '21 932 2834',
+    category:      'italian'
+  },
+
+  {
+    name:         'Cabana',
+    address:      'Estrela',
+    phone_number:  '21 942 2334',
+    category:      'belgian'
+  },
+
+  {
+    name:         'Osteria',
+    address:      'Alfama',
+    phone_number:  '21 832 2334',
+    category:      'italian'
+  },
+
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
