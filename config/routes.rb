3667b9fa775ab17e275ec50241eff737434eeac8
ruben bernardes <rubenmbernardes@gmail.com>
2018-03-14 12:52:58 +0100

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :restaurants do
+  namespace :admin do
+    resources :restaurants, :reviews, only: [:edit, :update, :destroy]
+  end
+  resources :restaurants, only: [:index, :new, :create, :show] do
     resources :reviews, only: [:new, :create]
   end
-  resources :reviews, only: [:show, :edit, :update, :destroy]
+  resources :reviews, only: [:show]
 end
